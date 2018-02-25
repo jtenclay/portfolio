7b13cf9473eb6223c0648d62bd769ed92de242cd
Jackson Tenclay <jptenclay@gmail.com>
2018-02-25 10:49:13 -0600

    Background dots done but choppy

--- src/scripts/backgrounds.js
+++ src/scripts/backgrounds.js
@@ -12,42 +12,39 @@ export function defaultInit() {
 
 	backgroundIllustration.innerHTML = '<div class="dots-mask dots-mask--left"></div><div class="dots-mask dots-mask--right"></div><div class="dots-mask dots-mask--top"></div><div class="dots-mask dots-mask--bottom"></div>';
 
-	let dotsWrapper = document.createElement("div");
+	let dotsWrapper = document.createElement("div"),
+			alternatingRowFlag = true;
 
-	for (let i = 0; i < 100; i = i + 10) {
+	for (let i = 0; i < 95; i = i + 8.66) {
 		for (let j = 5; j < 100; j = j + 10) {
 			let dot = document.createElement("div");
 			dot.style.top = i + '%';
-			dot.style.left = j + '%';
+			dot.style.left = (alternatingRowFlag) ? j + '%' : j + 5 + '%';
 			dot.className = 'default-dot';
 			dotsWrapper.appendChild(dot);
 		}
+		alternatingRowFlag = !alternatingRowFlag;
 	}
 
 	dotsWrapper.className = 'dots-wrapper';
 	backgroundIllustration.appendChild(dotsWrapper);
 
 	let dots = document.querySelectorAll('.default-dot');
-	function wrapAround(num) {
-		switch (true) {
-			case num < 0:
-				return num + 100;
-				break;
-			case num >= 100:
-				return num - 100;
-				break;
-			default:
-				return num;
-		}
-	}
 	function animateDots() {
 		dots.forEach((dot) => {
-			dot.style.top = wrapAround(parseFloat(dot.style.top) + 0.055) + '%';
-			dot.style.left = wrapAround(parseFloat(dot.style.left) - 0.15) + '%';
+			let wrappedTopFlag = false,
+					newTop = parseFloat(dot.style.top) + 0.055,
+					newLeft = parseFloat(dot.style.left) - 0.15;
+			if (newTop >= 100) {
+				wrappedTopFlag = true;
+				newTop -= 95.26;
+			}
+			dot.style.top = newTop + '%';
+			dot.style.left = (newLeft < 0 ? newLeft + 100 : newLeft) + (wrappedTopFlag ? 5 : 0) + '%';
 		});
 		timeout = setTimeout(() => {
 			animateDots();
-		}, 16.6);
+		}, 76.6);
 	}
 	animateDots();
 
