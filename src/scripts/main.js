85fca9c648d8e12833db14801fa8d484a814a642
Jackson Tenclay <jptenclay@gmail.com>
2018-02-24 19:54:36 -0600

    Background illustration position pt 1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,3 +5,29 @@ var arrayOfLinks = document.querySelectorAll(".work-links");
 arrayOfLinks.forEach((el) => {
 	// attach mouseover and mouseout listeners
 });
+
+// keep background illustration a square
+
+const $backgroundIllustrationWrapper = document.querySelector('.background-illustration__inner-wrapper'),
+			$backgroundIllustration = document.querySelector('.background-illustration');
+
+function resizeBackgroundIllustration() {
+	let size = Math.min($backgroundIllustrationWrapper.clientHeight, $backgroundIllustrationWrapper.clientWidth);
+	$backgroundIllustration.style.width = $backgroundIllustration.style.height = size + 'px';
+}
+resizeBackgroundIllustration();
+
+
+// resize handler
+
+window.addEventListener("resize", resizeThrottler, false);
+var resizeTimeout;
+function resizeThrottler() {
+  if ( !resizeTimeout ) {
+    resizeTimeout = setTimeout(function() {
+      resizeTimeout = null;
+      // here they go!
+      resizeBackgroundIllustration();
+    }, 66);
+  }
+}
