d961de518fe97e61fd69b62bdf49f6798107074b
Jackson Tenclay <jptenclay@gmail.com>
2018-02-25 11:56:01 -0600

    Background dots complete

--- src/scripts/backgrounds.js
+++ src/scripts/backgrounds.js
@@ -3,50 +3,12 @@ import * as tileGame from './tile-game';
 let timeout;
 
 const backgroundIllustration = document.querySelector('.background-illustration'),
-			backgroundTransition = 200; // change in css as well!
+			backgroundTransition = 300; // change in css as well!
 
 export function defaultInit() {
-	setTimeout(() => {
-		backgroundIllustration.classList.remove('hide');
-	}, backgroundTransition);
+	backgroundIllustration.classList.remove('hide');
 
-	backgroundIllustration.innerHTML = '<div class="dots-mask dots-mask--left"></div><div class="dots-mask dots-mask--right"></div><div class="dots-mask dots-mask--top"></div><div class="dots-mask dots-mask--bottom"></div>';
-
-	let dotsWrapper = document.createElement("div"),
-			alternatingRowFlag = true;
-
-	for (let i = 0; i < 95; i = i + 8.66) {
-		for (let j = 5; j < 100; j = j + 10) {
-			let dot = document.createElement("div");
-			dot.style.top = i + '%';
-			dot.style.left = (alternatingRowFlag) ? j + '%' : j + 5 + '%';
-			dot.className = 'default-dot';
-			dotsWrapper.appendChild(dot);
-		}
-		alternatingRowFlag = !alternatingRowFlag;
-	}
-
-	dotsWrapper.className = 'dots-wrapper';
-	backgroundIllustration.appendChild(dotsWrapper);
-
-	let dots = document.querySelectorAll('.default-dot');
-	function animateDots() {
-		dots.forEach((dot) => {
-			let wrappedTopFlag = false,
-					newTop = parseFloat(dot.style.top) + 0.055,
-					newLeft = parseFloat(dot.style.left) - 0.15;
-			if (newTop >= 100) {
-				wrappedTopFlag = true;
-				newTop -= 95.26;
-			}
-			dot.style.top = newTop + '%';
-			dot.style.left = (newLeft < 0 ? newLeft + 100 : newLeft) + (wrappedTopFlag ? 5 : 0) + '%';
-		});
-		timeout = setTimeout(() => {
-			animateDots();
-		}, 76.6);
-	}
-	animateDots();
+	backgroundIllustration.innerHTML = '<div class="dots-wrapper"></div>';
 
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,7 @@ module.exports = {
             {
               loader: 'css-loader',
               options: {
+                url: false,
                 minimize: true,
                 sourceMap: true
               }
