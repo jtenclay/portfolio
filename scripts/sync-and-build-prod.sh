8f17cbd758bc048fff98064ce9d402f6e2eabc1f
Jackson Tenclay <jptenclay@gmail.com>
2020-04-03 07:29:55 -0500

    Allow prod scripts to be run in cron

--- scripts/sync-and-build-prod.sh
+++ scripts/sync-and-build-prod.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ruby scripts/music_log_scripts.rb
+bundle exec ruby scripts/music_log_scripts.rb
 node scripts/create-d3-charts.js
-middleman build
+bundle exec middleman build
 rsync -avz ./build/ /var/www/jacksontenclay.com/html
