dea8e239508a895d1193939453e3e7ffdbd5dfb8
Jackson Tenclay <jptenclay@gmail.com>
2020-08-29 18:28:30 -0500

    Update build script for prod

--- scripts/sync-and-build-prod.sh
+++ scripts/sync-and-build-prod.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
+rm -rf ./build
+rm -rf ./.tmp
 bundle exec ruby scripts/music_log_scripts.rb
 node scripts/create-d3-charts.js
 bundle exec middleman build
-rm -rf ./build
-rm -rf ./.tmp
 rsync -avz ./build/ /var/www/jacksontenclay.com/html
